a14868a8a381fe5c47e09c7e4582044b3a3b538a
widura26 <widurategalrejo@gmail.com>
2023-12-16 12:39:37 +0700

    commit

---
+++ belajarPython/while.py
@@ -0,0 +1,20 @@
+i = 1;
+while i <= 6 :
+    print(i)
+    i += 1
+
+#break statement
+index = 1
+while index < 6 :
+    print(index)
+    if index == 3 :
+        break
+    index += 1
+
+#continue statement
+ind = 0
+while ind < 6 :
+    ind += 1
+    if ind == 3 :
+        continue
+    print(ind)
